769687cca02ec1d34474d575047ae8b7fa590bad
nabil <bilakhmissi@gmail.com>
2024-09-04 09:20:48 +0100

    redirect user in landing based on the role

--- front/src/app/pages/landing/landing.component.ts
+++ front/src/app/pages/landing/landing.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Subject, take, takeUntil, tap } from 'rxjs';
 import { LayoutService } from 'src/app/layout/service/app.layout.service';
 import { AuthService } from 'src/app/services/auth.service';
 
@@ -8,15 +8,35 @@ import { AuthService } from 'src/app/services/auth.service';
     selector: 'app-landing',
     templateUrl: './landing.component.html'
 })
-export class LandingComponent{
+export class LandingComponent implements OnInit, OnDestroy{
+
+    homeRedirectUrl = '';
+    private destroy$: Subject<void> = new Subject<void>();
 
     authenticated$ =  this._auth.isAuthenticated$;
+    authUser$ = this._auth.authenticatedUser$.pipe(
+        takeUntil(this.destroy$),
+        tap((user : any) => {
+            if(user){
+                this.homeRedirectUrl = user.role === "ADMIN" ? 'admin' : user.role === "AUDITOR" ? 'auditor' : 'client' ;
+            }
+        })
+    );
 
     constructor(
         public layoutService: LayoutService, 
         public router: Router,
         private _auth : AuthService
     ) { }
+
+    ngOnInit(): void {
+      this.authUser$.subscribe();  
+    }
+
+    ngOnDestroy(): void {
+        this.destroy$.next();
+        this.destroy$.complete();
+    }
 
     logout(){
         this._auth.logout()
