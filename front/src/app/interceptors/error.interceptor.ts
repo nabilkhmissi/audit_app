ca20ef8e186019cde6df967dff567fd21bfc9006
nabil <bilakhmissi@gmail.com>
2024-09-04 14:50:19 +0100

    clean code

--- front/src/app/interceptors/error.interceptor.ts
+++ front/src/app/interceptors/error.interceptor.ts
@@ -5,9 +5,8 @@ import {
   HttpEvent,
   HttpInterceptor,
 } from "@angular/common/http";
-import { Observable, of, throwError } from "rxjs";
-import { catchError, tap } from "rxjs/operators";
-import { ToastService } from "../services/toast.service";
+import { Observable, throwError } from "rxjs";
+import { catchError } from "rxjs/operators";
 import { AuthService } from "../services/auth.service";
 import { MessageService } from "primeng/api";
 
@@ -15,7 +14,6 @@ import { MessageService } from "primeng/api";
 export class ErrorInterceptor implements HttpInterceptor {
   constructor(
     private authServ: AuthService,
-    private _toast : ToastService,
     private _message : MessageService,
   ) {}
 
@@ -24,20 +22,12 @@ export class ErrorInterceptor implements HttpInterceptor {
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(
-      
-      //complete if 403 or 401 redirect to login and logout
-
-      // tap((res : any) => {
-      //   if(res.status == 200 || res.status == 201){
-      //     this._message.add({severity:'success', summary: 'Success', detail: res.body.message})
-      //   }
-      // }), 
       catchError((err) => {
         if (err.status === 401) {
           this.authServ.logout();
         }
         this._message.add({severity:'error', summary: 'Error', detail: err.error.message})
-        return throwError(err);;
+        return throwError(err);
       })
     );
   }
